639de8a9ceb99e7b85437e85048b7781f15123e5
Manmeet Oberoi <manmeet.s.oberoi@gmail.com>
2020-08-07 02:04:50 +0530

    Updates

---
+++ precache-manifest.1c5c04ed69406d0f1d8cfe30568c699e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f6ae914ad3707f2747a7bf40d2d3e36",
+    "url": "/github-profile-clone/index.html"
+  },
+  {
+    "revision": "359da80e59938fbaf83c",
+    "url": "/github-profile-clone/static/css/main.cbb84fb2.chunk.css"
+  },
+  {
+    "revision": "44ae5c0c8b3e9cd2ff48",
+    "url": "/github-profile-clone/static/js/2.0bc4338a.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/github-profile-clone/static/js/2.0bc4338a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "359da80e59938fbaf83c",
+    "url": "/github-profile-clone/static/js/main.34a2cebf.chunk.js"
+  },
+  {
+    "revision": "fcd0163f220e6a6974c0",
+    "url": "/github-profile-clone/static/js/runtime-main.73f6317b.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/github-profile-clone/static/media/logo.8dad8028.svg"
+  }
+]);
